a2fc467b8152787359e6555f3eba5828dcdfd7f7
Maicon Dias <maiconlotel@gmail.com>
2022-09-22 01:54:52 -0300

    Client repository created

---
+++ GerenciadorDeClientes.API/GerenciadorDeClientes.API.Domain/Repositories/IClientRepository.cs
@@ -0,0 +1,15 @@
+﻿using GerenciadorDeClientes.API.Domain.Entities;
+using System;
+using System.Collections.ObjectModel;
+
+namespace GerenciadorDeClientes.API.Domain.Repositories
+{
+    public interface IClientRepository
+    {
+        Task<Client> GetByIdAssync(int id);
+        Task<Collection<Client>> GetClientAsync();
+        Task<Client> CreateAsync(Client client);
+        Task EditAsync(Client client);
+        Task DeleteAsync(Client client);
+    }
+}
